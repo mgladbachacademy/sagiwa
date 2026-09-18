0bcbb769789ed8b9051818e506beeb00e8210431
ovarz <ovarz_03@yahoo.com>
2026-09-18 14:12:08 +0700

    000

--- index.php
+++ index.php
@@ -29,6 +29,68 @@
   
   </div>
 </section>
+  
+  
+  
+  
+  
+<section class="section-default section-tipeA section-tipeA2 section-about content-center">
+  <div class="section-container">
+    <div class="tipeA-img">
+      <picture class="tipeA-img-frame img-frame thumb-loading">
+        <img alt="Meet SAMUSA Consultant" class="lazyload" data-original="template/img/about.jpg"/>
+      </picture>
+    </div>
+    <div class="tipeA-content">
+      <div class="tipeA-header">
+        <h2 class="text-title tipeA-title">A space to pause, fell, and reconnect.</h2>
+      </div>
+      <div class="tipeA-info">
+        <p>Dalam deru kehidupan sehari-hari yang serba cepat, sering kali kita terjebak pada ilusi bahwa kita harus selalu bergerak dan produktif. Namun, ada kalanya jiwa dan raga kita tidak lagi membutuhkan tambahan rutinitas. Yang sejatinya kita butuhkan adalah sebuah jeda yang bermakna, sebuah ruang aman untuk benar-benar berhenti sejenak, menarik napas panjang, mendengarkan bahasa tubuh yang mungkin kelelahan, dan memeluk kembali segala emosi yang tengah berproses di dalam diri.</p>
+        <p>Berangkat dari kerinduan akan ketenangan tersebut, <b>Ruang Sadar Sagiwa by SANUSTRA</b> hadir sebagai oase praktik mindfulness dan wellbeing di kawasan BSD. Tempat ini dirancang secara khusus untuk menjadi rumah singgah bagi batin Anda; sebuah wadah yang merangkul Anda untuk kembali terhubung secara utuh dengan diri sendiri melalui harmoni olah tubuh (movement), kesadaran napas (breath), terapi suara (sound), hingga ruang untuk perenungan mendalam (reflection).</p>
+      </div>
+    </div>
+  </div>
+</section>
+  
+  
+  
+  
+  
+<section class="section-default section-tipeA section-tipeA1 section-address content-center">
+  <div class="section-container">
+    <div class="tipeA-img">
+      <div class="tipeA-img-frame img-frame thumb-loading">
+        <iframe class="lazyload" data-original="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1982.9185004060696!2d106.6648043366294!3d-6.285146137903389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fb0018bb0c0f%3A0x9efbf23c8c2bb4ad!2sBorussia%20M%C3%B6nchengladbach%20Academy%20Indonesia%20Office%20%26%20Cafe!5e0!3m2!1sen!2sid!4v1789714273324!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+      </div>
+    </div>
+    <div class="tipeA-content">
+      <div class="tipeA-header">
+        <h2 class="text-title tipeA-title">Contact us</h2>
+      </div>
+      <div class="tipeA-info">
+        <ul class="address-list">
+          <li class="address-icon">
+            <?php require ($_SERVER['SGW'].'template/img/icon/maps.svg')?>
+          </li>
+          <li class="address-info">
+            <p>Ruko BSD Junction Blok B.31,</p> 
+            <p>JL. Pahlawan Seribu, Tangerang Selatan,</p> 
+            <p>Banten 15322</p>
+          </li>
+        </ul>
+        <ul class="address-list">
+          <li class="address-icon">
+            <?php require ($_SERVER['SGW'].'template/img/icon/footer-instagram.svg')?>
+          </li>
+          <li class="address-info">
+            <a title="Instagram" class="address-link" href="https://www.instagram.com/ruangsadarsagiwa/">#ruangsadarsagiwa</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
 
 
 
